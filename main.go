ae81aec4089fc1e54e885a0a8c0dd56c1780b3d7
Myriam <myriamalbustany@gmail.com>
2024-12-19 18:28:42 +0200

    created visualization for board

--- main.go
+++ main.go
@@ -40,6 +40,87 @@ type Board struct {
 	Turn Color
 }
 
+// create the board
+func NewBoard() *Board {
+    board := &Board{
+        Turn: Black, // black is always the staring piece
+    }
+
+    // fill up board with black pieces
+    for y:= 0; y < 3; y++ {
+        for x:= 0; x < 8; x++ {
+            if ((x+y) % 2) != 0 {
+                board.Grid[y][x] = &Piece{
+                    Type: Normal,
+                    Color: Black,
+                }
+            }
+        }
+    }
+
+    // fill up board with red pieces
+    for y:= 0; y < 3; y++ {
+        for x:= 0; x < 8; x++ {
+            if ((x+y) % 2) != 0 {
+                board.Grid[y][x] = &Piece{
+                    Type: Normal,
+                    Color: Red,
+                }
+            }
+        }
+    }
+
+    return board
+}
+
+
+func (b *Board) VisualizeBoard() {
+    for y:= 0; y < 8; y++ {
+        fmt.Printf("%d |", y)
+        for x:= 0; x < 8; x++ {
+            // check if it is empty
+            // what color is that space
+            if b.Grid[y][x] == nil {
+                if (x+y) % 2 == 0 {
+                    fmt.Print(" . |")
+                } else {
+                    fmt.Print("   |")
+                }
+            } else {
+                // if the space is not empty:
+                // check what color piece it is
+                // check whether piece is normal or queen
+
+                // this space is NOT empty
+                piece := b.Grid[y][x]
+
+                if piece.Color == Black {
+                    if piece.Type == Queen {
+                        // black ,queen
+                        fmt.Print(" B |")
+                    } else {
+                        // black, regular
+                        fmt.Print(" b |")
+                    }
+                } else {
+                    if piece.Type == Queen {
+                        // red, queen
+                        fmt.Print(" R |")
+                    } else {
+                        // red, regular
+                        fmt.Print(" r |")
+                    }
+                }
+            }
+        }
+
+        fmt.Println("\n  +---+---+---+---+---+---+---+---+")
+    }
+}
+
+
 func main() {
-	fmt.Println("Hello world!")
+    board := NewBoard()
+    board.VisualizeBoard()
+
 }
